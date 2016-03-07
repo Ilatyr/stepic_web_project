67dcf38d3743dfbebb217d82732c18520849bed3
Ilatyr <xilatyrx@gmail.com>
2016-03-07 14:00:46 +0300

    фыв

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
+#sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
+#sudo /etc/init.d/gunicorn restart
 
-#cd $PWD/web/
+cd $PWD/web/
 #gunicorn -c /etc/gunicorn.d/hello.py -b 0.0.0.0:8080 hello:app
-#gunicorn -c /etc/gunicorn.d/hello.py hello:app
+gunicorn -c /etc/gunicorn.d/hello.py hello:app
