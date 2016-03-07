2ad19390a29714aab0a15e04de50ceae26dc6be3
Ilatyr <xilatyrx@gmail.com>
2016-03-07 13:50:58 +0300

    зззз

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart
 
 cd $PWD/web/
