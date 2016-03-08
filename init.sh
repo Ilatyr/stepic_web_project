f2061dc1ca1c8bc2129ae7b1f8e12769bb046453
Vadim M. Moiseev <xilatyrx@gmail.com>
2016-03-08 19:30:39 +0300

    sad

--- init.sh
+++ init.sh
@@ -4,6 +4,5 @@ sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
 #sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart
 
-cd $PWD/web/
 #gunicorn -c /etc/gunicorn.d/hello.py -b 0.0.0.0:8080 hello:app
 gunicorn -D -c /etc/gunicorn.d/hello.py hello
