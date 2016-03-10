69b3643be29a04846ef0f0d46d7e16dd57ca75fc
Ilatyr <xilatyrx@gmail.com>
2016-03-10 12:23:04 +0300

    !!!!

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^signup/$', views.test),
     url(r'^question/(\d+)/$', views.test),
     url(r'^ask/$', views.test),
-    url(r'^ask/popular/$', views.test)
+    url(r'^ask//popular/$', views.test),
     url(r'^popular/$', views.test),
     url(r'^new/$', views.test),
 ]
