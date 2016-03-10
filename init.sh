edc898e7d6dd0eb89e639ea2c41636703f42948a
Ilatyr <xilatyrx@gmail.com>
2016-03-10 10:58:24 +0300

    3

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/guni-django.conf   /etc/gunicorn.d/guni-django.conf
 #sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart
 
 #gunicorn -c /etc/gunicorn.d/hello.py -b 0.0.0.0:8080 hello:app
 gunicorn -D -c /etc/gunicorn.d/hello.py hello
+gunicorn -D -c /etc/gunicorn.d/guni-django.conf
